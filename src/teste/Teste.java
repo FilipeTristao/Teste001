38e572b7ab1173edd655167edcd12bddc319e706
filipe_damasceno <filipe_damasceno@10.1.48.185>
2014-05-27 14:21:06 -0300

    Inicialização do projeto

---
+++ src/teste/Teste.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teste;
+
+/**
+ *
+ * @author filipe_damasceno
+ */
+public class Teste {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("testestestestestetestestetestestesteste");
+    }
+}
